8802bc3bafd3e0722ee603d6d908352d6fbd99bf
Dalton Tewanger <daltontewanger@yahoo.com>
2024-01-30 21:31:28 -0800

    Updated admin.js for functionality to change book availibility amounts

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,40 @@
 
-// Your Code Here
+// can use same main() functrions from index.js to bring the book info over
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    let div = document.querySelector('#root');
+    div.style.listStyleType = 'none';
+
+    books.forEach(bookQuantity)
+}
+
+function bookQuantity(book) {
+    let div = document.querySelector('#root')
+    let li = document.createElement('li')
+    li.textContent = book.title
+    let availableInput = document.createElement('input')
+    availableInput.value = book.quantity
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: availableInput.value
+            })
+        })
+    })
+    li.append(availableInput, saveButton)
+
+    div.append(li)
+}
+
+main();
